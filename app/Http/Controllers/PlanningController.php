062699de31da49454a21b9e5f4e966c770e0ec09
Cedric <limacedric@hotmail.fr>
2022-01-26 20:39:05 +0100

    test spout

--- app/Http/Controllers/PlanningController.php
+++ app/Http/Controllers/PlanningController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Planning\importHubRequest;
 use App\Models\Hub;
+use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Storage;
 
@@ -12,6 +13,24 @@ class PlanningController extends Controller
 
     public function show (Hub $hub)
     {
+        $filePath = Storage::path('planning/Gujan/planning-2022.xlsx');
+        $reader = ReaderEntityFactory::createXLSXReader();
+        $reader->open($filePath);
+
+        foreach ($reader->getSheetIterator() as $sheet) {
+            if ($sheet->getName() === ' PLANNING ') {
+                $object = collect();
+                foreach ($sheet->getRowIterator() as $row) {
+                    $cells = $row->getCells();
+                        if (!empty($cells[1]->getValue()) && $cells[1]->getType() === 5) {
+                            dd($cells);
+                            $timestamp = date("l d F", $cells[1]->getValue()->getTimestamp());
+                            dd($cells[3]);
+                        }
+                }
+                break;
+            }
+        }
 
         if (Storage::get('planning/Gujan/planning-2022.xlsx')) {
             dd('ok');
