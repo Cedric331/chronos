218c0dae81692cc7d1bae2288c9e14d001af2afe
Cedric <limacedric@hotmail.fr>
2022-03-26 10:52:24 +0100

    pull

--- public/js/app.js
+++ public/js/app.js
@@ -25390,7 +25390,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n.checkbox:checked + .check-icon[data-v-3202a656] {\n    display: flex;\n}\n\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n.checkbox:checked + .check-icon[data-v-3202a656] {\r\n    display: flex;\n}\r\n\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
