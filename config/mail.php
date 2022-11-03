6df88280037ec2c6943e9f2a9f6d397601d418e4
Cedric <limacedric@hotmail.fr>
2022-11-03 14:55:53 +0100

    correction mail

--- config/mail.php
+++ config/mail.php
@@ -46,8 +46,8 @@ return [
             'stream' => [
                 'ssl' => [
                     'allow_self_signed' => true,
-                    'verify_peer' => false,
-                    'verify_peer_name' => false,
+                    'verify_peer' => true,
+                    'verify_peer_name' => true,
                 ],
             ],
         ],
